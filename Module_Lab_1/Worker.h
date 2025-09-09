bb1fc16b081c1b48f6b907e4597284b3496e9c7c
Sakha-kz <sakhtai.erdaulet@gmail.com>
2025-09-09 07:48:24 +0500

    Модуль 01 Лабораторная работа

--- Module_Lab_1/Worker.h
+++ Module_Lab_1/Worker.h
@@ -2,7 +2,25 @@
 #define Worker_H
 
 #include "Employee.h"
+using namespace std;
 
+class Worker : public Employee {
+private:
+    double hourlyRate;
+    int hoursWorked;
+public:
+    Worker(const string& n, int i, const string& r, double hourlyRate, int hoursWorked)
+        : Employee(n, i, r), hourlyRate(hourlyRate), hoursWorked(hoursWorked) {}
 
+    double getHourlyRate() const { return hourlyRate; }
+    void setHourlyRate(double rate) { hourlyRate = rate; }
+
+    int getHoursWorked() const { return hoursWorked; }
+    void setHoursWorked(int hours) { hoursWorked = hours; }
+
+    double money() const override {
+        return hourlyRate * hoursWorked;
+    }
+};
 
 #endif
--- Module_Lab_1/main.cpp
+++ Module_Lab_1/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include "Module_Lab_1.h"
+#include <iomanip>
+#include "Employee.h"
+#include "Worker.h"
 
 using namespace std;
 
 int main() {
+    Worker w("Серик", 1, "Инженер", 50.0, 160);
+    w.display();
 
+    Employee* e = new Worker("Берик", 2, "Программист", 50.5, 160);
+    e->display();
+    delete e;
+
+    cout << fixed << setprecision(2);
     return 0;
 }
